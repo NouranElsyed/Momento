e8aa52c01f12ee9356a517e4e844d47b865264ca
unknown <es-nouranelsayed54@alexu.edu.eg>
2025-09-11 21:24:17 +0300

    loading spinner

--- src/Routes/ProtectedRoute.tsx
+++ src/Routes/ProtectedRoute.tsx
@@ -1,18 +1,22 @@
 import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useGetUserQuery } from "../app/features/api/apiSlice";
-
+import { PropagateLoader } from "react-spinners";
 interface IProps {
   children: ReactNode;
   redirectTo: string;
   forPublic: boolean;
 }
+
 const ProtectedRoute = ({ children, forPublic, redirectTo }: IProps) => {
   const { data, isLoading } = useGetUserQuery(null);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+if (isLoading) {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <PropagateLoader color="#2b83da" size={25} />
+    </div>
+  );
+}
   // if (!(!!data&&forPublic)) return <Navigate to={redirectTo} />;
   if (!data) {
     if (forPublic) {
