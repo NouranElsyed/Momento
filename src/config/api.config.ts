1ee5a04634215f7af20f02edd3b9b248ead30b7e
Nouran Elsayed <153534623+NouranElsyed@users.noreply.github.com>
2026-07-22 09:44:22 +0300

    Update API base URL to new endpoint

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://linked-posts.routemisr.com",
+  baseURL: "https://route-posts.routemisr.com",
+  //baseURL: "https://linked-posts.routemisr.com",
+  
 });
 export default api;
